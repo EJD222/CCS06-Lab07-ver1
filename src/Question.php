b5026c5d90fea6b4d93a8cff5e8668e41572a8b3
Elmalia Jane Diaz <diaz.elmaliajane@auf.edu.ph>
2023-05-13 11:58:10 +0800

    Fixed Code Indentations

--- src/Question.php
+++ src/Question.php
@@ -4,35 +4,35 @@ namespace App;
 
 class Question
 {
-	protected $number;
-	protected $question;
-	protected $choices;
-	protected $answer;
+    protected $number;
+    protected $question;
+    protected $choices;
+    protected $answer;
 
-	public function __construct($number, $question, $choices, $answer)
-	{
-		$this->number = $number;
-		$this->question = $question;
-		$this->choices = $choices;
-		$this->answer = $answer;
-	}
+    public function __construct($number, $question, $choices, $answer)
+    {
+        $this->number = $number;
+        $this->question = $question;
+        $this->choices = $choices;
+        $this->answer = $answer;
+    }
 
-	public function getNumber()
-	{
-		return $this->number;
-	}
+    public function getNumber()
+    {
+        return $this->number;
+    }
 
-	public function getQuestion()
-	{
-		return $this->question;
-	}
+    public function getQuestion()
+    {
+        return $this->question;
+    }
 
-	public function getChoices()
-	{
-		return $this->choices;
-	}
+    public function getChoices()
+    {
+        return $this->choices;
+    }
 
-	public function getAnswer()
+    public function getAnswer()
     {
         return $this->answer;
     }
